a87f7fd74a592a26bb040d11ab8db42db476b872
Martin Goldhahn <mgoldhahn@gmail.com>
2014-11-22 18:42:29 +0100

    use query parameter for user

--- logindata.js
+++ logindata.js
@@ -1,15 +1,24 @@
 var http = require('http');
 var UtmpParser = require('utmp');
+var url = require('url');
 
 http.createServer(function (req, res) {
 	res.writeHead(200, {'Content-Type': 'application/json'});
-        var p = new UtmpParser('/var/log/wtmp');
+  var reqUrl = url.parse(req.url, true, false);
+  if (!reqUrl.query.user) {
+    res.end();
+    return;
+  }
+
 	var wtmpList = [];
-	p.on('data', function(d) {
-		wtmpList.push(d);
+  var p = new UtmpParser('/var/log/wtmp');
+ 	p.on('data', function(d) {
+ 	    if (d.user == reqUrl.query.user && d.type == "USER_PROCESS") {
+ 	      wtmpList.push(d);
+ 	    }
 	});
 	p.on('end', function() {
-		res.end(JSON.stringify(wtmpList[0]));
+		res.end(JSON.stringify(wtmpList));
 	});
 	p.run();
 }).listen(8088, "0.0.0.0");
